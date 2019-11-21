6a738e9c70c260c37952cfe25e4b468845acf6bc
Luis gabriel <luisgabriel_ace@hotmail.com>
2019-11-21 14:45:12 -0300

    lgx angular components

---
+++ lgx-angular-components/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TableModule } from "./components/table/table.module";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
